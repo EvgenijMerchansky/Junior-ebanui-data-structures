0ad9a98f9355dfd9fef7b958d37bd92e2082b654
EvgenijMerchansky <evgenij.merchansky@gmail.com>
2018-07-17 21:56:47 +0300

    feat: add deque structure

--- Deque/Deque.cs
+++ Deque/Deque.cs
@@ -1,5 +1,105 @@
 using System;
 
-class Deque {
+class Deque<T> {
+    private T[] Store = new T[0];
+    bool CheckFirst = false;
+    private void ToEnlarge () 
+    {
+        T[] UpdatedStore = new T[this.Store.Length + 1];
+        for (int I = 0; I < this.Store.Length; I++) 
+        {
+            UpdatedStore[I] = Store[I];
+        }
+        this.Store = UpdatedStore;
+    }
 
+    private void ElementsMigration (T Item)
+    {
+        T[] UpdatedStore = new T[this.Store.Length];
+        for (int I = 0; I < this.Store.Length; I++) 
+        {
+            try
+            {
+                UpdatedStore[I + 1] = Store[I];
+            } 
+            catch (IndexOutOfRangeException Ex) 
+            {
+                continue;
+            }
+        }
+        UpdatedStore[0] = Item;
+        this.Store = UpdatedStore;
+    }
+
+    private void ToReduce ()
+    {
+        T[] UpdatedStore = new T[this.Store.Length - 1];
+        for (int I = 0; I < this.Store.Length; I++) 
+        {
+            try 
+            {
+                UpdatedStore[I] = Store[I];
+            } 
+            catch (IndexOutOfRangeException Ex) 
+            {
+                continue;
+            }
+            
+        }
+        this.Store = UpdatedStore;
+    }
+
+    private void ToUnshift ()
+    {
+        T[] UpdatedStore = new T[this.Store.Length - 1];
+        for (int I = 0; I < this.Store.Length; I++) 
+        {
+            try 
+            {
+                UpdatedStore[I] = Store[I + 1];
+            } 
+            catch (IndexOutOfRangeException Ex) 
+            {
+                continue;
+            }
+            
+        }
+        this.Store = UpdatedStore;
+    }
+
+    public T TakeLast() 
+    {
+        return this.Store[this.Store.Length - 1];
+    }
+
+    public T TakeFirst() 
+    {
+        return this.Store[0];
+    }
+
+    public void Push(T Item) 
+    {
+        this.ToEnlarge ();
+        this.Store[this.Store.Length - 1] = Item;
+    }
+
+    public void Shift(T Item) 
+    {
+        ToEnlarge ();
+        ElementsMigration(Item);
+    }
+
+    public void Pop() 
+    {
+        this.ToReduce();
+    }
+
+    public void Unshift() 
+    {
+        this.ToUnshift();
+    }
+
+    public int DequeSize() {
+        return this.Store.Length;
+    }
 }
--- Program.cs
+++ Program.cs
@@ -6,17 +6,17 @@ namespace structures_c_
     {
      static void Main(string[] args)
         {
-            LinkedLIsts<int> Lists = new LinkedLIsts<int>();
-            Lists.Add(12);
-            Lists.Add(6);
-            Lists.Add(64);
-            Lists.Add(124);
-            Lists.Add(256);
-            Lists.Add(512);
-            Lists.Add(1084);
-            System.Console.WriteLine(Lists.Size() + " Lists Size");
-            Lists.GetChain();
-            Lists.Remove(0);
+            // LinkedLIsts<int> Lists = new LinkedLIsts<int>();
+            // Lists.Add(12);
+            // Lists.Add(6);
+            // Lists.Add(64);
+            // Lists.Add(124);
+            // Lists.Add(256);
+            // Lists.Add(512);
+            // Lists.Add(1084);
+            // // System.Console.WriteLine(Lists.Size() + " Lists Size");
+            // Lists.GetChain();
+            // Lists.Remove(0);
 
             CustomQueue<int> Queuery = new CustomQueue<int>();
             Queuery.Add(14);
@@ -35,6 +35,46 @@ namespace structures_c_
             StackOnArray.Pop();
             StackOnArray.Push(30);
             StackOnArray.Push(35);
+
+            Deque<int> DeDeque = new Deque<int>();
+            DeDeque.Push(15);
+            DeDeque.Push(20);
+            DeDeque.Push(25);
+            
+            // get initial size:
+            // System.Console.WriteLine(DeDeque.DequeSize() + " size start");
+
+            // should be return * 15 *
+            // System.Console.WriteLine(DeDeque.TakeFirst());
+
+            // remove one:
+            // DeDeque.Pop();
+
+            // should be return * 15 *
+            // System.Console.WriteLine(DeDeque.TakeFirst());
+
+            // Shift method testing...
+            DeDeque.Shift(11);
+
+            DeDeque.Push(30);
+            DeDeque.Shift(7);
+            DeDeque.Pop();
+            System.Console.WriteLine(DeDeque.TakeLast());
+            // should be return * 11 *
+            // System.Console.WriteLine(DeDeque.TakeFirst());
+            System.Console.WriteLine(DeDeque.DequeSize() + " size start");
+            System.Console.WriteLine(DeDeque.TakeFirst());
+            DeDeque.Unshift();
+            System.Console.WriteLine(DeDeque.TakeFirst());
+            System.Console.WriteLine(DeDeque.DequeSize() + " size FINISH");
+            DeDeque.Shift(173);
+            System.Console.WriteLine(DeDeque.TakeFirst());
+            DeDeque.Push(190);
+            System.Console.WriteLine(DeDeque.TakeFirst());
+            System.Console.WriteLine(DeDeque.TakeLast());
+
+            // get updated size: * 3 *
+            // System.Console.WriteLine(DeDeque.DequeSize() + " size finish");
         }
     }
 }
